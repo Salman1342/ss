7bc2d70e9668763839516baf498d01958b925674
Salman1342 <115477905+Salman1342@users.noreply.github.com>
2023-02-09 22:43:58 -0800

    Add files via upload

---
+++ port.js
@@ -0,0 +1,16 @@
+const nav =document.getElementById("#")
+
+var scroll = new SmoothScroll('a[href*="#"]',{
+                        
+    speed:1000,
+    speedAsDuration:true
+});
+
+
+var typed =new Typed(".auto-type",{
+    strings:["FRONT END DEVELOPER"," COME FROM CHENNAI","MOHAMED SALMAN"],
+    typeSpeed:150,
+    backSpeed:150,
+    loop:true
+});
+
